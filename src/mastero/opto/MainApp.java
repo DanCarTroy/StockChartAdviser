9e2dd45c5d38368fd5bcdd9675a4df2eab218e23
Zhiheng Zhu <LucienChu@Computer.(none)>
2017-02-24 21:52:20 -0500

    first try

--- src/mastero/opto/MainApp.java
+++ src/mastero/opto/MainApp.java
@@ -1,25 +1,39 @@
-package mastero.opto;
-	
+package view;
+
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 
 public class MainApp extends Application {
+	private Stage primaryStage;
+	private BorderPane mainLayout;
+
 	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+	public void start(Stage primaryStage) throws Exception {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("ProfitUs");
+		showMainView();
 	}
-	
+
+	public void showMainView()throws IOException{
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(MainApp.class.getResource("MainWindow.fxml"));
+		Scene scene = new Scene(loader.load());
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
+	public void showChartWindow() throws IOException {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(MainApp.class.getResource("Main/LineChart/LineChartView.fxml"));
+		Scene scene = loader.load();
+	}
+
+
 	public static void main(String[] args) {
 		launch(args);
 	}
