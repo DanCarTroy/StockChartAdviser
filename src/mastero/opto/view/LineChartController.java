afd352fcde8464b30382fc9b860e8dd7c2d71743
Zhiheng Zhu <LucienChu@Computer.(none)>
2017-03-03 11:19:58 -0500

    small change

--- src/mastero/opto/view/LineChartController.java
+++ src/mastero/opto/view/LineChartController.java
@@ -2,8 +2,14 @@ package mastero.opto.view;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
+import javafx.scene.control.Button;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import mastero.opto.view.MainApp;
 import java.io.IOException;
 
@@ -15,35 +21,61 @@ public class LineChartController {
     private static final int ONE_YEAR = 365;
     private static final int TWO_YEAR = 730;
     private static final int FIVE_YEAR = 1825;
-
-    @FXML
-    LineChart<String, Number>  lineChart;
+    @FXML Button allDataButton;
+    @FXML Button oneYearDataButton;
+    @FXML Button twoYearDataButton;
+    @FXML Button fiveYearDataDataButton;
+    @FXML Button smaButton;
+    @FXML LineChart<String, Number>  lineChart;
     @FXML
     private void showMainFrame() throws IOException{
         main.showMainWindow();
     }
 
-    public void allDatabtn(ActionEvent event){
-        lineChart.getData().clear();
+    public void allDatabtn()throws IOException{
         XYChart.Series data = main.getChartData();
-        lineChart.getData().add(data);
+        if (main.getChartData().getData().size() == 0)
+            showErroPopUp();
+        else {
+            lineChart.getData().clear();
+            lineChart.getData().add(data);
+        }
     }
 
-    public void oneYearDatabtn(ActionEvent event) {
+    public void oneYearDatabtn() throws IOException{
         lineChart.getData().clear();
         XYChart.Series data = main.getChartData(ONE_YEAR);
-        lineChart.getData().add(data);
+        if (data.getData().size() == 0)
+            showErroPopUp();
+        else
+            lineChart.getData().add(data);
     }
 
-    public void twoYearDatabtn(ActionEvent event) {
+    public void twoYearDatabtn(ActionEvent event) throws IOException{
         lineChart.getData().clear();
         XYChart.Series data = main.getChartData(TWO_YEAR);
-        lineChart.getData().add(data);
+        if (data.getData().size() == 0)
+            showErroPopUp();
+        else
+            lineChart.getData().add(data);
     }
 
-    public void fiveYearDatabtn(ActionEvent event) {
+    public void fiveYearDatabtn(ActionEvent event) throws IOException{
         lineChart.getData().clear();
         XYChart.Series data = main.getChartData(FIVE_YEAR);
-        lineChart.getData().add(data);
+        if (data.getData().size() == 0)
+            showErroPopUp();
+        else
+            lineChart.getData().add(data);
+    }
+
+    public void showErroPopUp()throws IOException{
+        Stage stage;
+        Parent root;
+        stage = new Stage();
+        root = FXMLLoader.load(getClass().getResource("errorPopUp.fxml"));
+        stage.setScene(new Scene(root));
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.showAndWait();
     }
 }
