8a015892a6d4ad43b07b19bdab04a63b9e32a370
Zhiheng Zhu <LucienChu@Computer.(none)>
2017-03-31 14:13:32 -0400

    AK47

--- src/mastero/opto/view/LineChartController.java
+++ src/mastero/opto/view/LineChartController.java
@@ -1,5 +1,6 @@
 package mastero.opto.view;
 
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -8,6 +9,7 @@ import javafx.scene.Scene;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import mastero.opto.MainApp;
@@ -21,12 +23,19 @@ public class LineChartController {
     private static final int ONE_YEAR = 365;
     private static final int TWO_YEAR = 730;
     private static final int FIVE_YEAR = 1825;
-    XYChart.Series<String, Number> sma = new XYChart.Series<String, Number>();
+    XYChart.Series<String, Number> sma20 = new XYChart.Series<String, Number>();
+    XYChart.Series<String, Number> sma50 = new XYChart.Series<String, Number>();
+    XYChart.Series<String, Number> sma100 = new XYChart.Series<String, Number>();
+    XYChart.Series<String, Number> sma200 = new XYChart.Series<String, Number>();
     @FXML Button allDataButton;
     @FXML Button oneYearDataButton;
     @FXML Button twoYearDataButton;
     @FXML Button fiveYearDataDataButton;
     @FXML Button smaButton;
+    @FXML CheckBox sma_20;
+    @FXML CheckBox sma_50;
+    @FXML CheckBox sma_100;
+    @FXML CheckBox sma_200;
     @FXML LineChart<String, Number>  lineChart;
     @FXML
     private void showMainFrame() throws IOException{
@@ -34,11 +43,12 @@ public class LineChartController {
     }
 
     public void allDatabtn()throws IOException{
+        lineChart.getData().clear();
         XYChart.Series data = main.getChartData();
         if (main.getChartData().getData().size() == 0)
             showErroPopUp();
         else {
-            lineChart.getData().clear();
+            data.setName("All data");
             lineChart.getData().add(data);
         }
     }
@@ -48,8 +58,10 @@ public class LineChartController {
         XYChart.Series data = main.getChartData(ONE_YEAR);
         if (data.getData().size() == 0)
             showErroPopUp();
-        else
+        else {
+            data.setName("1 years data");
             lineChart.getData().add(data);
+        }
     }
 
     public void twoYearDatabtn(ActionEvent event) throws IOException{
@@ -57,8 +69,10 @@ public class LineChartController {
         XYChart.Series data = main.getChartData(TWO_YEAR);
         if (data.getData().size() == 0)
             showErroPopUp();
-        else
+        else {
+            data.setName("2 years data");
             lineChart.getData().add(data);
+        }
     }
 
     public void fiveYearDatabtn(ActionEvent event) throws IOException{
@@ -66,8 +80,10 @@ public class LineChartController {
         XYChart.Series data = main.getChartData(FIVE_YEAR);
         if (data.getData().size() == 0)
             showErroPopUp();
-        else
+        else {
+            data.setName("5 years data");
             lineChart.getData().add(data);
+        }
     }
 
     public void showErroPopUp()throws IOException{
@@ -80,43 +96,70 @@ public class LineChartController {
         stage.showAndWait();
     }
 
-    public void showSMA20(ActionEvent event) throws IOException{
-        lineChart.getData().removeAll(sma);
+    public void showSMA20(){
         DataAnalysis analysis = new DataAnalysis(main.getCurrentSeries());
-        sma = analysis.SMA20();
-        if (sma.getData().size() == 0)
-            showErroPopUp();
-        else
-            lineChart.getData().add(sma);
+        sma20 = analysis.SMA20();
+        sma20.setName("SMA 20");
+        if (sma20.getData().size() != 0)
+            lineChart.getData().add(sma20);
+        System.out.println(sma20.getData().size());
     }
 
-    public void showSMA50(ActionEvent event) throws IOException{
-        lineChart.getData().removeAll(sma);
+
+    public void showSMA50 (){
         DataAnalysis analysis = new DataAnalysis(main.getCurrentSeries());
-        sma = analysis.SMA50();
-        if (sma.getData().size() == 0)
-            showErroPopUp();
-        else
-            lineChart.getData().add(sma);
+        sma50 = analysis.SMA50();
+        sma50.setName("SMA 50");
+        if (sma50.getData().size() != 0)
+            lineChart.getData().add(sma50);
+        System.out.println(sma50.getData().size());
     }
 
-    public void showSMA100(ActionEvent event) throws IOException{
-        lineChart.getData().removeAll(sma);
+
+    public void showSMA100(){
         DataAnalysis analysis = new DataAnalysis(main.getCurrentSeries());
-        sma = analysis.SMA100();
-        if (sma.getData().size() == 0)
-            showErroPopUp();
-        else
-            lineChart.getData().add(sma);
+        sma100 = analysis.SMA100();
+        sma100.setName("SMA 100");
+        if (sma100.getData().size() != 0)
+            lineChart.getData().add(sma100);
+        System.out.println(sma100.getData().size());
     }
 
-    public void showSMA200(ActionEvent event) throws IOException{
-        lineChart.getData().removeAll(sma);
+    public void showSMA200(){
         DataAnalysis analysis = new DataAnalysis(main.getCurrentSeries());
-        sma = analysis.SMA200();
-        if (sma.getData().size() == 0)
-            showErroPopUp();
+        sma200 = analysis.SMA200();
+        sma200.setName("SMA 200");
+        if (sma200.getData().size() != 0)
+            lineChart.getData().add(sma200);
+        System.out.println(sma200.getData().size());
+    }
+
+
+    public void handle20Checkboxes(){
+        if(sma_20.isSelected())
+            showSMA20();
         else
-            lineChart.getData().add(sma);
+            lineChart.getData().remove(sma20);
+    }
+
+    public void handle50Checkboxes(){
+        if(sma_50.isSelected())
+            showSMA50();
+        else
+            lineChart.getData().remove(sma50);
+    }
+
+    public void handle100Checkboxes(){
+        if(sma_100.isSelected())
+            showSMA100();
+        else
+            lineChart.getData().remove(sma100);
+    }
+
+    public void handle200Checkboxes(){
+        if(sma_200.isSelected())
+            showSMA200();
+        else
+            lineChart.getData().remove(sma200);
     }
 }
